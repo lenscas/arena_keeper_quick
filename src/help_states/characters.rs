22c318a047219150e6529644df912f67eed1ef49
lenscas <lenscas@gmail.com>
2019-06-28 14:10:28 +0200

    Fix wasm build

--- src/help_states/characters.rs
+++ src/help_states/characters.rs
@@ -4,7 +4,7 @@ use crate::structs::{
 	Character
 };
 use quicksilver::lifecycle::Window;
-use rayon::prelude::*;
+
 
 #[derive(Default)]
 pub struct Characters {
@@ -18,17 +18,32 @@ impl Characters {
 			time_until_spawn : 10
 		}
 	}
+	#[cfg(target_arch = "wasm32")]
+	fn update_paralel(&mut self, grid : &Field) {
+		let mut rng = rand::thread_rng();
+		self.characters.iter_mut().for_each(
+			|v| v.calc_path(grid,&mut rng)
+				
+		);
+	}
+	#[cfg(not(target_arch = "wasm32"))]
+	fn update_paralel(&mut self, grid : &Field) {
+		use rayon::prelude::*;
+		self.characters.par_iter_mut().for_each_init(
+			rand::thread_rng,
+			|rng,v| v.calc_path(grid,rng)
+				
+		);
+	} 
 	pub fn update(&mut self, grid : &Field) {
 		self.time_until_spawn -= 1;
 		if self.time_until_spawn == 0 {
 			self.time_until_spawn = 5;
 			self.characters.push(Character::new());
 		}
-		self.characters.par_iter_mut().for_each_init(
-			rand::thread_rng,
-			|rng,v| v.calc_path(grid,rng)
-				
-		);
+		
+		
+		self.update_paralel(grid);
 		self.characters.iter_mut().for_each(|v|v.update(grid));
 	}
 	pub fn render(&mut self, cam : &CameraWork, window : &mut Window) {
