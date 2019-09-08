599062eedaea19c41345c4b1524ebd01f37db009
lenscas <lenscas@gmail.com>
2019-09-08 19:38:01 +0200

    Fixed gen all script to include tiles

--- scripts/generators/all.ts
+++ scripts/generators/all.ts
@@ -1,7 +1,10 @@
-export default (params : string[]) => {
+export default (params: string[]) => {
     console.log("generating species")
     require("./species").default([]);
+    console.log("generating tiles");
+    require("./tiles").default([]);
     console.log("generating assets")
     require("./assets").default([]);
-    
+
+
 }
