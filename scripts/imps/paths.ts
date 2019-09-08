3cbd11f6b4d5b9f00c0cbd2add9a2fa0c7ed375c
lenscas <lenscas@gmail.com>
2019-09-08 19:37:49 +0200

    Changed location of data for generation

--- scripts/imps/paths.ts
+++ scripts/imps/paths.ts
@@ -1,9 +1,9 @@
-import {normalize} from "path"
+import { normalize } from "path"
 export const currentFolder = __dirname.split("/")
-export const appendFile = (...args: string[]) => normalize(currentFolder.map(v=>v).join("/")+"/"+args.join("/"))
-export const generatedFolder = appendFile("..","..","src","generated")
-export const assetDir = appendFile("..", "..","static")
-export const speciesFolder = appendFile("..","..","species")
-export const generatedAssetsFolder = appendFile("..", "..","static","generated")
-export const tileDir = appendFile("..","..","tiles")
-export const schemaFolder = appendFile("..","types")
+export const appendFile = (...args: string[]) => normalize(currentFolder.map(v => v).join("/") + "/" + args.join("/"))
+export const generatedFolder = appendFile("..", "..", "src", "generated")
+export const assetDir = appendFile("..", "..", "static")
+export const speciesFolder = appendFile("..", "..", "data", "species")
+export const generatedAssetsFolder = appendFile("..", "..", "static", "generated")
+export const tileDir = appendFile("..", "..", "data", "tiles")
+export const schemaFolder = appendFile("..", "types")
