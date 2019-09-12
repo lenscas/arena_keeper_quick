cd05017fb99d48768d1be8dac0b89f2abe1abc00
lenscas <lenscas@gmail.com>
2019-09-12 12:47:51 +0200

    Fix load_all not returning dyn

--- scripts/templates/loadAssets.ts
+++ scripts/templates/loadAssets.ts
@@ -1,6 +1,6 @@
 import { firstToUpper } from "../imps/str";
 
-export const makeFile = (toExecute : string) => `
+export const makeFile = (toExecute: string) => `
 use quicksilver::Future;
 use quicksilver::graphics::Image;
 use super::loaded::AssetManager;
@@ -9,29 +9,29 @@ use quicksilver::graphics::Font;
 use super::loaded::Images;
 use super::loaded::Fonts;
 
-pub fn load_all() -> Box<Future<Item=AssetManager, Error=Error>>{
+pub fn load_all() -> Box<dyn Future<Item=AssetManager, Error=Error>>{
 	let mut manager = AssetManager::new();
 	Box::new(
 		${toExecute}
 	)
 }
 `
-export const templateImage = (files: Array<{enumName : string, fileName : string,into : string, enumContainer: string}>) => {
-	return files.map( (image,k) => {
+export const templateImage = (files: Array<{ enumName: string, fileName: string, into: string, enumContainer: string }>) => {
+	return files.map((image, k) => {
 		let basic = `
 ${firstToUpper(image.into)}::load("${image.fileName}").and_then(|v| {
 	manager.insert_${image.into}(${image.enumContainer}::${image.enumName},v);
 	Ok(manager)
 })`
-	if(k < files.length-1) {
-		console.log("in here?")
-		if(k==0){
-			basic = basic + `.and_then(|mut manager|{`
-		} else {
-			basic = basic + `}).and_then(|mut manager|{`
+		if (k < files.length - 1) {
+			console.log("in here?")
+			if (k == 0) {
+				basic = basic + `.and_then(|mut manager|{`
+			} else {
+				basic = basic + `}).and_then(|mut manager|{`
+			}
 		}
-	}
-	return basic
+		return basic
 	}).join("") + "})"
 }
 /*
