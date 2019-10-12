74bfa917439468a9000bd6e28e2f941f35f0a11e
lenscas <lenscas@gmail.com>
2019-10-13 01:15:07 +0200

    Remove files needed for code generation

--- scripts/generate.ts
+++
@@ -1,5 +0,0 @@
-const generatorName = process.argv[2]
-const otherArgs = process.argv.filter((v,k)=> k > 2)
-console.log(generatorName)
-const generatorInUse = require("./generators/"+generatorName)
-generatorInUse.default()
