ffa5137b1caa265c6940137c154cece8be615bcc
lenscas <lenscas@users.noreply.github.com>
2019-11-17 16:27:05 +0000

    Automated deployment: Sun Nov 17 16:27:05 UTC 2019 da0da6eb5152ea44e42fdaae67215e32305af799

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ if( typeof Rust === "undefined" ) {
     } else if( typeof module === "object" && module.exports ) {
         module.exports = factory();
     } else {
-        Rust.develop = factory();
+        Rust.main = factory();
     }
 }( this, function() {
     return (function( module_factory ) {
@@ -19,13 +19,13 @@ if( typeof Rust === "undefined" ) {
         if( typeof process === "object" && typeof process.versions === "object" && typeof process.versions.node === "string" ) {
             var fs = require( "fs" );
             var path = require( "path" );
-            var wasm_path = path.join( __dirname, "develop.wasm" );
+            var wasm_path = path.join( __dirname, "main.wasm" );
             var buffer = fs.readFileSync( wasm_path );
             var mod = new WebAssembly.Module( buffer );
             var wasm_instance = new WebAssembly.Instance( mod, instance.imports );
             return instance.initialize( wasm_instance );
         } else {
-            var file = fetch( "develop.wasm", {credentials: "same-origin"} );
+            var file = fetch( "main.wasm", {credentials: "same-origin"} );
 
             var wasm_instance = ( typeof WebAssembly.instantiateStreaming === "function"
                 ? WebAssembly.instantiateStreaming( file, instance.imports )
@@ -39,11 +39,11 @@ if( typeof Rust === "undefined" ) {
             return wasm_instance
                 .then( function( wasm_instance ) {
                     var exports = instance.initialize( wasm_instance );
-                    console.log( "Finished loading Rust wasm module 'develop'" );
+                    console.log( "Finished loading Rust wasm module 'main'" );
                     return exports;
                 })
                 .catch( function( error ) {
-                    console.log( "Error loading Rust wasm module 'develop':", error );
+                    console.log( "Error loading Rust wasm module 'main':", error );
                     throw error;
                 });
         }
