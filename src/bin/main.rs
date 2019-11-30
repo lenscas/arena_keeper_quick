f609ca5f61e2f4ea7d9bece3bfc7209f84c22b61
lenscas <lenscas@gmail.com>
2019-12-01 00:01:41 +0100

    Fix double loading of assets

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,5 @@
-use arena::{
-    assets::to_load::load_all,
-    modules::{
-        handle_files::{get_all_mod_paths, load_mod_info},
-        structs::Module,
-    },
-    states::StateManager,
-};
+use arena::{assets::to_load::load_all, states::StateManager};
 use quicksilver::{
-    combinators::join_all,
     geom::Vector,
     graphics::Color,
     lifecycle::{run, Asset, Event, Settings, State, Window},
@@ -17,20 +9,15 @@ use std::{rc::Rc, sync::Mutex};
 
 pub struct MainState<'a> {
     assets: Asset<StateManager<'a>>,
-    test: Asset<Vec<Module>>,
 }
 impl State for MainState<'static> {
     fn new() -> Result<Self> {
-        let b = get_all_mod_paths()
-            .and_then(|v| join_all(v.iter().map(|x| load_mod_info(x)).collect::<Vec<_>>()));
         Ok(Self {
             assets: Asset::new(load_all().and_then(|v| StateManager::new(v))),
-            test: Asset::new(b),
         })
     }
     fn draw(&mut self, window: &mut Window) -> Result<()> {
         let test = Rc::new(Mutex::new(window));
-        self.test.execute(|_| Ok(()))?;
         self.assets.execute_or(
             |state| {
                 let mut b = test.lock().unwrap();
